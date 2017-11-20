d5512b4ffce909dcabadc455b7c31d98b8238f64
Paul Mylchreest <paul.mylchreest@mac.com>
2017-11-20 18:12:07 -0500

    Added aliases for gpg functions

--- init/misc_settings.el
+++ init/misc_settings.el
@@ -1,4 +1,4 @@
-(set-frame-font "AnonymousPro-18")
+(set-frame-font "Anonymous Pro-18")
 
 ;; https://www.masteringemacs.org/article/beginners-guide-to-emacs
 
@@ -9,9 +9,17 @@
 (ido-mode 1)
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
-(menu-bar-mode -1)
+;; (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (setq column-number-mode t)
 
 (setq multi-term-program "/bin/bash")
+
+(setq tab-width 2)
+
+;; aliases for gpg encrypt/decryption
+(defalias 'sign 'epa-sign-region)
+(defalias 'encrypt 'epa-encrypt-region)
+(defalias 'decrypt 'epa-decrypt-region)
+(defalias 'verify 'epa-verify-region)
